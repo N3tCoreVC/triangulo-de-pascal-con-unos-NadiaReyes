66f3ab76c9d0f79d760116cac066e69717fb364e
NadiaReyes <nadia.reyes@institutolisi.org>
2018-09-21 18:13:05 -0500

    subo pero debo terminar

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ namespace triangulo_de_pascal_con_unos_NadiaReyes
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(int n){
+            for(int i:n;if>-1;i--){
+                int espacios = i;
+                while(espacios-- >0){Console.Write("")}
+                int unos = n-i;
+                while (unos-- >0){Console.Write("1 ")}
+                Console.Write();
+        }
         }
     }
 }
